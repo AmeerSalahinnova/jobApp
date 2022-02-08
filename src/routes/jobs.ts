082a877122febfc601a610ee4e8ee15f5f7a6187
ameer78 <ameersalah78@gmail.com>
2022-02-08 22:05:28 +0200

    git ignore

--- src/routes/jobs.ts
+++ src/routes/jobs.ts
@@ -5,6 +5,7 @@ const jobsRouter = express.Router();
 import * as jobsController from "../controllers/jobs";
 /* GET users listing. */
 jobsRouter.get("/", jobsController.getJobs);
+jobsRouter.get("/:id", jobsController.getJobByID);
 jobsRouter.post("/", jobsController.createJob);
 
 export default jobsRouter
