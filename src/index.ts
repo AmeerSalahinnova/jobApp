e37234fbd8633fc2c347dfc383d8b83b025fff96
ameer78 <ameersalah78@gmail.com>
2022-02-09 23:32:38 +0200

    important updates

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,19 @@ import express from "express";
 import path from "path";
 import jobsRouter from "./routes/jobs";
 import { responseHelper } from "./shared/responseHelper";
+import cors from "cors"
+
 const app = express();
 const port = 8080; // default port to listen
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/jobs', jobsRouter);
 
-console.log(__dirname)
 // Configure Express to use EJS
 app.set( "views", path.join( __dirname, "/views" ) );
 app.set('view engine', 'ejs');
